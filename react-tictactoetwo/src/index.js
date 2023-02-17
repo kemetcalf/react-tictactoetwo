63cf09c5ab65bebfdbd9267fe86a3652e44b0762
Kat <kemetcalf3.14@gmail.com>
2023-02-17 13:42:00 -0800

    Rewrite Board component with two loops instead of hardcoding

--- react-tictactoetwo/src/index.js
+++ react-tictactoetwo/src/index.js
@@ -12,7 +12,8 @@ function Square(props) {
 
 class Board extends React.Component {
 	renderSquare(i) {
-		// console.log(i);
+		console.log(i);
+
 		return (
 			<Square
 				value={this.props.squares[i]}
@@ -22,24 +23,20 @@ class Board extends React.Component {
 	}
 
 	render() {
+		let gameBoard = [];
+		for (let row = 0; row < 3; row++) {
+			let boardRow = [];
+			for (let col = 0; col < 3; col++) {
+				boardRow.push(
+					<span key={row * 3 + col}>{this.renderSquare(row * 3 + col)}</span>
+				);
+			}
+			gameBoard.push(<div key={row}>{boardRow}</div>);
+		}
+
 		return (
 			<div>
-				{/* <div className="status">{status}</div> */}
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+				<div>{gameBoard}</div>
 			</div>
 		);
 	}
